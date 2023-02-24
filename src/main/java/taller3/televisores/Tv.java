b23af687a2b8467d8a3a8377bdfb83a518891d53
AbrahaMD1010 <miguelabraham490@gmail.com>
2023-02-23 22:49:32 -0500

    atributos terminados

--- src/main/java/taller3/televisores/Tv.java
+++ src/main/java/taller3/televisores/Tv.java
@@ -1,8 +1,51 @@
-public class Tv {
+public class TV {
     private Marca marca;
-    private int canal;
-    private int precio;
+    private int canal=1;
+    private int precio=500;
     private boolean estado;
-    private int volumen;
+    private int volumen=1;
     private Control control;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca=marca;
+        this.estado=estado;
+    }
+
+    //setters and getters
+    public void setMarca(Marca marca) {
+        this.marca=marca;
+    }
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setControl(Control control) {
+        this.control=control;
+    }
+    public Control getControl() {
+        return control;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio=precio;
+    }
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen=volumen;
+    }
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setCanal(int canal) {
+        this.canal=canal;
+    }
+    public int getCanal() {
+        return canal;
+    }
 }
+
+

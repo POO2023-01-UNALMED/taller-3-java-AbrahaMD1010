1605a227343cbdfa09a68293cbb854ce305b7824
AbrahaMD1010 <amiguel@unal.edu.co>
2023-02-26 22:01:13 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -12,7 +12,7 @@ public class TV {
     public TV(Marca marca, boolean estado) {
         this.marca=marca;
         this.estado=estado;
-        TV.numTV=numTV++;
+
     }
 
     //setters and getters
